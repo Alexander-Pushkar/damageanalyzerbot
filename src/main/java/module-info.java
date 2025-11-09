c21514b90f20849a70a69fc3e2d227aec89cdd12
AlexanderPushkarPC <aleksandr.pushkar@mail.ru>
2025-11-09 14:22:44 +0300

    created part of the project structure for fast start telegram bot and google vision

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.car.damageanalyzerbot {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.car.damageanalyzerbot to javafx.fxml;
-    exports com.car.damageanalyzerbot;
-}
